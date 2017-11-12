c945a352d84ef5b51fbce4c1582d8415905c60c9
Vladislav Yarmak <vladislav@vm-0.com>
2017-11-12 16:42:16 +0200

    WIP

--- det.py
+++ det.py
@@ -6,25 +6,32 @@ import numpy as np
 import time
 import signal
 import skimage.measure as measure
+import logging
 
 
 TRESHOLD = 5
+BLUR_RADIUS = 4
+
+
+gaussian_ksize = (BLUR_RADIUS,) * 4
+
+
+def setup_logger(level=logging.INFO):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level)
+    log_handler = logging.StreamHandler(sys.stderr)
+    log_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(log_handler)
+    return logger
 
 
 def exit_handler(signum, frame):
     sys.exit(0)
 
 
-def main():
-    signal.signal(signal.SIGINT, exit_handler)
-
-    cv2.namedWindow("preview")
-    vc = cv2.VideoCapture(0)
-    vc.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1920)
-    vc.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 960)
-    vc.set(cv2.cv.CV_CAP_PROP_FPS, 30)
-
+def processing_loop(vc, logger):
     ctr = 0
+    out = 0
 
     if vc.isOpened(): # try to get the first frame
         rval, frame = vc.read()
@@ -39,12 +46,25 @@ def main():
 
         labels, count = measure.label(bw, return_num=True)
         ctr += count
-        ##ctr += (gray > 5).sum()
-        #ctr += np.count_nonzero(frame[:, :] > TRESHOLD)
         new_time = time.time()
-        print ctr, "fps=%.2f" % (1/(new_time - prev_time))
+        logger.debug("count=%d fps=%.2f res=%s", ctr, (1/(new_time - prev_time)), frame.shape[:-1])
+        if count:
+            cv2.imwrite("out_%.2d.bmp" % (out,), frame)
+            out += 1
         prev_time = new_time
         rval, frame = vc.read()
+
+
+def main():
+    signal.signal(signal.SIGINT, exit_handler)
+    logger = setup_logger(logging.DEBUG)
+
+    vc = cv2.VideoCapture(0)
+    vc.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1920)
+    vc.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 960)
+    vc.set(cv2.cv.CV_CAP_PROP_FPS, 30)
+
+    processing_loop(vc, logger)
 
 
 if __name__ == '__main__':
